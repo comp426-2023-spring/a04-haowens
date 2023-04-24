02636ad9e3a24d1158e265f57368cbcea31ff556
Hayley <haowens@email.unc.edu>
2023-04-24 01:03:33 -0400

    idk

--- server.js
+++ server.js
@@ -17,20 +17,24 @@ app.get('/app', (req, res) => {
 
 //Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}. (HINT: regex)
 app.get('/app/rps', (req, res) => {
-    res.send(rps());
+    res.status(200).send(JSON.Stringify(rps()));
 })
 
 //Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
-app.get('/app/rps', (req, res) => {
-    res.send(rpsls());
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.Stringify(rpsls()));
 })
 
 //Endpoint /app/rps/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as data bodies and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
 app.get('/app/rps/play', () => {
-
+    res.status(200).send(JSON.Stringify(rps(req.body.shot)));
 })
 
 //Endpoint /app/rpsls/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rpsls/play', () => {
+    res.status(200).send(JSON.Stringify(rpsls(req.body.shot)));
+})
+
 
 //Endpoint /app/rpsls/play/(rock|paper|scissors)/ should return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
 
