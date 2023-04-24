aa3bd117e8150f0281740acf9022e18ad464c38d
Hayley <haowens@email.unc.edu>
2023-04-24 00:48:49 -0400

    hit 100

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const http = require("http"); 
-const fs = require("fs");
+#!/usr/bin/env node
+import {rps, rpsls} from "./lib/rpsls.js";
+const express = require('express');
+const app = express();
+
+// const http = require("http"); 
+// const fs = require("fs");
 
 const minimist = require('minimist');
-const args = minimist(process.argv.slice()); 
+const args = minimist(process.argv.slice(2)); 
 const port = args["port"] || 5000;
+app.listen(port);
+
+
+app.get('/app', (req, res) => {
+	res.status(200).send("200 OK");
+})
+
 
 const host = 'localhost';
 
