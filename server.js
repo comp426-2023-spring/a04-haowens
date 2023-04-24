cfc38c8b49fa931835d768ec56618f097f4696e4
Hayley <haowens@email.unc.edu>
2023-04-24 00:54:37 -0400

    hundred

--- server.js
+++ server.js
@@ -11,22 +11,25 @@ const args = minimist(process.argv.slice(2));
 const port = args["port"] || 5000;
 app.listen(port);
 
-
+//Check endpoint at /app/ that returns 200 OK.
 app.get('/app', (req, res) => {
 	res.status(200).send("200 OK");
 })
 
+//Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}. (HINT: regex)
+app.get('/app/rps', (req, res) => {
+    res.send(rps());
+})
 
-const host = 'localhost';
+//Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
+app.get('/app/rps', (req, res) => {
+    res.send(rpsls());
+})
 
-fs.readFile('./public/index.html', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  http.createServer(function (req, res)  {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(data);
-    console.log(`Server listening on port ${port}`);
-  }).listen(port);
-});
+//Endpoint /app/rps/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as data bodies and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+app.get('/app/rps/play', () => {
+
+})
+
+//Endpoint /app/rpsls/play/ should accept request bodies in the following forms: shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) and return {"player":"(rock|paper|scissors)","opponent":"(rock|paper|scissors)","result":"(win|lose|tie)"}
+
