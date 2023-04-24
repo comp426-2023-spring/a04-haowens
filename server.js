938c130978e0ccd716cfdef7339ebbaab64b0174
Hayley <haowens@email.unc.edu>
2023-04-24 14:35:30 -0400

    urlencoded

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import {rps, rpsls} from "./lib/rpsls.js";
 import express from 'express';
 import minimist from 'minimist';
 const app = express();
+app.use(express.urlencoded({extended: true}));
 const args = minimist(process.argv.slice(2)); 
 const port = args["port"] || 5000;
 app.listen(port);
