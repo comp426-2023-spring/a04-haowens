2863b7104cfe925a571422ff9f016a2a90c2b828
Hayley <haowens@email.unc.edu>
2023-04-24 15:08:57 -0400

    express.json

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import minimist from 'minimist';
 const app = express();
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 const args = minimist(process.argv.slice(2)); 
 const port = args["port"] || 5000;
 app.listen(port);
